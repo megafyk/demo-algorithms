163d12a7afd44b4c99ce3c04b5d37057bcddfeda
megafyk <megafyk@gmail.com>
2019-05-01 11:42:54 +0700

    Add open the lock solution

--- src/myqueue/open_the_lock.py
+++ src/myqueue/open_the_lock.py
@@ -1,3 +1,22 @@
+import time
+
 class Solution(object):
     def openLock(self, deadends, target):
-        
+        moved, q, cnt, move = set(deadends), ['0000'], 0, {str(
+            i): [str((i+1) % 10), str((i-1) % 10)] for i in range(10)}
+        if '0000' in moved:
+            return -1 
+        while q:
+            new = []
+            cnt += 1
+            for code in q:
+                for i, c in enumerate(code):
+                    for curr in [code[:i] + move[c][0] + code[i+1:], code[:i] + move[c][1] + code[i+1:]]:
+                        if curr not in moved:
+                            if curr == target:
+                                return cnt
+                            new.append(curr)
+                            # node visited
+                            moved.add(curr)
+            q = new
+        return -1
